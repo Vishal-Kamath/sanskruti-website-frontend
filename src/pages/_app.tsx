618704c8394360ec602b66248c3ed23b0354a863
Vishal-Kamath <vishalkamath853@gmail.com>
2023-04-21 13:30:24 +0530

    🔄️redo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,6 @@
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-import Layout from '@/components/layout';
-import '@/styles/globals.css';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-// redux
-import { Provider } from 'react-redux';
-import { store } from '@/store';
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
-
-  return (
-    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
