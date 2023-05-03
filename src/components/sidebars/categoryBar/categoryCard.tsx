9375ad242a3a53ef9112bec15aa0cc4ab8bf610b
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-03 19:40:14 +0530

    ⬆️update: category card

--- src/components/sidebars/categoryBar/categoryCard.tsx
+++ src/components/sidebars/categoryBar/categoryCard.tsx
@@ -11,7 +11,11 @@ const CategoryCard: React.FC<{
   const [open, setOpen] = useState(false);
   const category = filters.find((filter) => filter.main === title);
   return (
-    <div className="group flex flex-shrink-0 overflow-hidden rounded-lg border-2 border-gray-300 bg-white max-sm:flex-col">
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="group flex flex-shrink-0 overflow-hidden rounded-lg border-2 border-gray-300 bg-white max-sm:flex-col"
+    >
       <div
         style={{
           backgroundImage: `url("${image}")`,
@@ -23,18 +27,17 @@ const CategoryCard: React.FC<{
         } aspect-square w-full overflow-hidden transition-all duration-300 ease-in-out sm:h-[20rem]`}
       >
         <div
-          onClick={() => setOpen(!open)}
           className={`absolute ${
             open ? 'top-0 h-full' : 'bottom-0 h-16'
           } flex w-full flex-col bg-white bg-opacity-40 py-2 font-semibold backdrop-blur-2xl`}
         >
           <span className="w-full text-center text-xl text-black">{title}</span>
           {open && (
-            <div className="flex max-h-full w-full flex-col gap-1 overflow-y-auto px-3 py-5 text-lg text-gray-700 scrollbar-none">
+            <div className="flex max-h-full w-full flex-col gap-1 overflow-y-auto px-3 py-5 text-lg text-black scrollbar-none">
               {category?.sub.map((subItem) => (
                 <Link
                   href={`${link}?${title}=${subItem.title}`}
-                  className="w-fit border-b-2 border-transparent pb-[2px] hover:border-gray-700"
+                  className="w-fit border-b-2 border-transparent hover:border-black hover:border-opacity-25"
                 >
                   {subItem.title}
                 </Link>
@@ -43,13 +46,14 @@ const CategoryCard: React.FC<{
           )}
         </div>
 
-        <div className="absolute bottom-4 right-4 grid h-8 w-8 place-content-center rounded-full bg-white text-black">
+        {/* <div className="absolute bottom-4 right-4 grid h-8 w-8 place-content-center rounded-full bg-white text-black">
           <FaAngleDown
+            onClick={() => setOpen(!open)}
             className={`${
               !open ? 'sm:rotate-180' : 'max-sm:rotate-180'
             } transition-all ease-in-out`}
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
