aedc61fee2c6e886ee9996537283d29e08265885
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-28 11:37:57 +0530

    ⬆️update: category card update

--- src/components/sidebars/categoryBar/categoryCard.tsx
+++ src/components/sidebars/categoryBar/categoryCard.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { filters } from '../filterBar/filtersList';
 import { FaAngleDown } from 'react-icons/fa';
+import UIButton from '@/components/common/button';
 
 const CategoryCard: React.FC<{
   image: string;
@@ -11,50 +12,50 @@ const CategoryCard: React.FC<{
   const [open, setOpen] = useState(false);
   const category = filters.find((filter) => filter.main === title);
   return (
-    <div
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-      className="group flex flex-shrink-0 overflow-hidden rounded-lg border-2 border-gray-300 bg-white max-sm:flex-col"
-    >
+    <div className="group relative flex h-fit flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border-2 border-amber-500 bg-white max-sm:flex-col">
       <div
+        onClick={() => setOpen((open) => !open)}
         style={{
           backgroundImage: `url("${image}")`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
         }}
-        className={`relative ${
+        className={` ${
           open ? 'h-[25rem]' : 'h-[10rem]'
         } aspect-square w-full overflow-hidden transition-all duration-300 ease-in-out sm:h-[20rem]`}
+      ></div>
+      <div
+        className={`absolute ${
+          open ? 'top-0 h-full' : 'bottom-0 h-16'
+        } flex w-full cursor-default flex-col bg-amber-100 py-2 font-semibold`}
       >
-        <div
-          className={`absolute ${
-            open ? 'top-0 h-full' : 'bottom-0 h-16'
-          } flex w-full flex-col bg-white bg-opacity-40 py-2 font-semibold backdrop-blur-2xl`}
-        >
-          <span className="w-full text-center text-xl text-black">{title}</span>
-          {open && (
-            <div className="flex max-h-full w-full flex-col gap-1 overflow-y-auto px-3 py-5 text-lg text-black scrollbar-none">
-              {category?.sub.map((subItem) => (
-                <Link
-                  href={`${link}?${title}=${subItem.title}`}
-                  className="w-fit border-b-2 border-transparent hover:border-black hover:border-opacity-25"
-                >
-                  {subItem.title}
-                </Link>
-              ))}
-            </div>
-          )}
-        </div>
-
-        {/* <div className="absolute bottom-4 right-4 grid h-8 w-8 place-content-center rounded-full bg-white text-black">
-          <FaAngleDown
-            onClick={() => setOpen(!open)}
-            className={`${
-              !open ? 'sm:rotate-180' : 'max-sm:rotate-180'
-            } transition-all ease-in-out`}
-          />
-        </div> */}
+        <span className="w-full text-center text-xl font-semibold text-black">
+          {title}
+        </span>
+        {open && (
+          <div className="flex max-h-full w-full flex-col gap-1 overflow-y-auto px-3 py-5 text-lg text-black scrollbar-none">
+            {category?.sub.map((subItem) => (
+              <Link
+                href={`${link}?${title}=${subItem.title}`}
+                className="w-fit border-b-2 border-transparent font-normal hover:underline"
+              >
+                {subItem.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
+
+      <UIButton
+        onClick={() => setOpen((open) => !open)}
+        className="absolute bottom-4 right-4 grid h-8 w-8 place-content-center rounded-full border-amber-500 bg-white text-black hover:outline-amber-200"
+      >
+        <FaAngleDown
+          className={`${
+            !open ? 'sm:rotate-180' : 'max-sm:rotate-180'
+          } transition-all ease-in-out`}
+        />
+      </UIButton>
     </div>
   );
 };
