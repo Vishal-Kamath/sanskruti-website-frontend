8cd68ccbfb0f25028c5c5e697207d25d4d0913f5
Vishal-Kamath <vishalkamath853@gmail.com>
2023-05-31 11:54:19 +0530

    ➕Add: UIButton

---
+++ src/components/common/button.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utils/lib";
+import { FC, HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {}
+const UIButton: FC<Props> = ({ className, children, ...props }) => {
+  return (
+    <button
+      className={cn(
+        "flex items-center justify-center rounded-md border-2 border-gray-500 py-2 hover:outline hover:outline-4 hover:outline-gray-300",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default UIButton;
