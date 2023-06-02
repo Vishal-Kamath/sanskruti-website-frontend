5bc77d4b607171cf6ab519a1dc0f00d5f6785721
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-02 19:56:44 +0530

    ⬆️update: minor changes in footer and header

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import FooterDetails from "./footerDetails";
+import Image from "next/image";
 
 const Footer: FC = () => {
   return (
     <footer className="mt-auto flex flex-col gap-3">
       <a href="" className="px-[5vw]" target="_blank">
-        <img
+        <Image
           src="/assets/footerInstagramImage.png"
           alt="Visit our instagram diaries"
+          width={300}
+          height={300}
           className="w-full"
         />
       </a>
