674ff9f6c326f9af0513190286e12ab47bc5c0d9
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-05 16:59:54 +0530

    ✨feature: edit user profile

--- src/app/user/details/edit/page.tsx
+++ src/app/user/details/edit/page.tsx
@@ -2,23 +2,78 @@
 
 import UIButton from "@/components/common/button";
 import { Input } from "@/components/common/input";
-import Container from "@/components/user/container";
+import Container from "@/app/user/components/container";
 import { selectUser } from "@/redux/slice/user.slice";
-import { useAppSelector } from "@/redux/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/store/hooks";
 import { FC, useState } from "react";
+import {
+  NotificationType,
+  setNotification,
+  showNotification,
+} from "@/redux/slice/notification.slice";
+import axios from "axios";
 
 const EditProfile: FC = () => {
   const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
 
   const [username, setUsername] = useState(user.username);
-  const [mobileNumber, setMobileNumber] = useState(user.Mobile_No);
+  const [mobileNumber, setMobileNumber] = useState(user.Mobile_No?.toString());
   const [email, setEmail] = useState(user.email);
 
+  const submit = async () => {
+    if (!username?.trim() || !mobileNumber?.trim() || !email?.trim()) {
+      dispatch(
+        setNotification({
+          message: "fill all details",
+          type: "warning",
+          content:
+            "We request the user to please fill all the required fields.",
+        })
+      );
+      return dispatch(showNotification());
+    }
+
+    await axios
+      .post<NotificationType>(
+        `${process.env.ENDPOINT}/api/v1/user/update`,
+        {
+          username,
+          Mobile_No: Number(mobileNumber),
+          email,
+        },
+        {
+          headers: {
+            "Authorization": `Bearer ${user.accessToken}`,
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        const response = res.data;
+        dispatch(
+          setNotification({ message: response.message, type: response.type })
+        );
+        dispatch(showNotification());
+      })
+      .catch((err) => {
+        const response = err.response.data;
+        dispatch(
+          setNotification({
+            message: response.message,
+            type: response.type,
+          })
+        );
+        dispatch(showNotification());
+      });
+  };
+
   return (
-    <div className="flex flex-col gap-3 w-full">
+    <div className="flex w-full flex-col gap-3">
       <Container containerTitle="Edit Profile Details">
-        <form className="flex flex-col gap-6 w-full max-w-lg">
-          <div className="flex flex-col w-full gap-2">
+        <div className="mx-auto flex w-full max-w-lg flex-col gap-6">
+          <div className="flex w-full flex-col gap-2">
             <span className="shrink-0 text-lg">Update username:</span>
             <Input
               input_type="text"
@@ -27,7 +82,7 @@ const EditProfile: FC = () => {
               setValue={setUsername}
             />
           </div>
-          <div className="flex flex-col w-full gap-2">
+          <div className="flex w-full flex-col gap-2">
             <span className="shrink-0 text-lg">Update email:</span>
             <Input
               input_type="text"
@@ -36,7 +91,7 @@ const EditProfile: FC = () => {
               setValue={setEmail}
             />
           </div>
-          <div className="flex flex-col w-full gap-2">
+          <div className="flex w-full flex-col gap-2">
             <span className="shrink-0 text-lg">Update mobile number:</span>
             <Input
               input_type="text"
@@ -46,10 +101,13 @@ const EditProfile: FC = () => {
             />
           </div>
 
-          <UIButton className="ml-auto w-fit px-16 bg-sky-100 hover:outline-sky-200">
+          <UIButton
+            onClick={submit}
+            className="ml-auto w-full bg-sky-100 hover:outline-sky-200"
+          >
             SUBMIT
           </UIButton>
-        </form>
+        </div>
       </Container>
     </div>
   );
