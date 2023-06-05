e91b6f7a5cd8ec909f40bc692c9f4ac82227588d
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-05 17:30:36 +0530

    ⬆️update: category bar

--- src/components/sidebars/categoryBar/categoryBar.tsx
+++ src/components/sidebars/categoryBar/categoryBar.tsx
@@ -2,69 +2,28 @@
 
 import { FC, useState, useEffect } from "react";
 import CategoryCard from "./categoryCard";
-import { useAppSelector } from "@/redux/store/hooks";
-import { selectSidebarOpen } from "@/redux/slice/sidebar.slice";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Autoplay } from "swiper";
 
-const categoryList: { image: string; title: string; link: string }[] = [
-  {
-    title: "Salwar Kameez",
-    image: "/temp/Salwar Kameez.png",
-    link: "/category/Salwar Kameez",
-  },
-  {
-    title: "Lehenga's",
-    image: "/temp/Lehenga's.png",
-    link: "/category/Lehenga's",
-  },
-  {
-    title: "Indo Western",
-    image: "/temp/Indo Western.png",
-    link: "/category/Indo Western",
-  },
-  {
-    title: "Bridal",
-    image: "/temp/Bridal.png",
-    link: "/category/Bridal",
-  },
-  {
-    title: "Kurti's",
-    image: "/temp/Kurti's.png",
-    link: "/category/Kurti's",
-  },
-  {
-    title: "Western Wear",
-    image: "/temp/Western Wear.png",
-    link: "/category/Western Wear",
-  },
-  {
-    title: "Dress Material's",
-    image: "/temp/Dress Material's.png",
-    link: "/category/Dress Material's",
-  },
-];
+import "swiper/css";
+import "swiper/css/autoplay";
+import { filters } from "@/data/filterlist";
 
 const CategoryBar: FC = () => {
-  const open = useAppSelector(selectSidebarOpen);
-
   const [numberSlides, setNumberSlides] = useState(4);
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth > 1024) {
+      if (window.innerWidth > 1280) {
+        setNumberSlides(5);
+      } else if (window.innerWidth > 1024) {
         setNumberSlides(4);
       } else if (window.innerWidth > 768) {
         setNumberSlides(3);
-      } else if (window.innerWidth > 640) {
-        setNumberSlides(2);
       } else {
-        setNumberSlides(3);
+        setNumberSlides(2);
       }
-
-      setIsMobile(window.innerWidth < 640);
     };
 
     handleResize();
@@ -76,35 +35,27 @@ const CategoryBar: FC = () => {
   }, []);
 
   return (
-    <div
-      className={`${
-        !open && "max-sm:hidden"
-      } isolate w-full bg-white px-[5vw] max-sm:fixed max-sm:left-0 max-sm:top-0 max-sm:z-30`}
-    >
-      <div className="flex flex-col gap-2 max-sm:max-h-screen max-sm:min-h-screen max-sm:overflow-y-auto max-sm:pt-36 max-sm:scrollbar-none">
-        <h3 className="border-b-2 border-gray-300 pb-2 text-xl font-semibold">
-          Categories
+    <div className="w-full bg-white px-[5vw]">
+      <div className="flex flex-col gap-3">
+        <h3 className="text-center font-ysabeau text-2xl font-semibold">
+          Shop by categories
         </h3>
-
-        {isMobile ? (
-          <div className="sm:custom_scrollbar flex w-full gap-2 py-2 max-sm:flex-col max-sm:pb-20 sm:overflow-x-auto">
-            {categoryList.map((category) => (
-              <CategoryCard key={category.title} {...category} />
-            ))}
-          </div>
-        ) : (
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={numberSlides}
-            className="flex w-full"
-          >
-            {categoryList.map((category) => (
-              <SwiperSlide key={category.title} className="w-fit">
-                <CategoryCard {...category} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={10}
+          slidesPerView={numberSlides}
+          className="flex w-full"
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: true,
+          }}
+        >
+          {filters.map((filter) => (
+            <SwiperSlide key={filter.main} className="w-fit">
+              <CategoryCard {...filter} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
--- src/components/sidebars/categoryBar/categoryCard.tsx
+++ src/components/sidebars/categoryBar/categoryCard.tsx
@@ -1,68 +1,29 @@
-"use client";
-
+import { FC } from "react";
+import Image from "next/image";
 import Link from "next/link";
-import { FC, useState } from "react";
-import { filters } from "@/data/filterlist";
-import { FaAngleDown } from "react-icons/fa";
-import UIButton from "@/components/common/button";
-import clsx from "clsx";
 
 interface Props {
   image: string;
-  title: string;
+  main: string;
   link: string;
 }
-const CategoryCard: FC<Props> = ({ title, image, link }) => {
-  const [open, setOpen] = useState(false);
-  const category = filters.find((filter) => filter.main === title);
+const CategoryCard: FC<Props> = ({ main, image, link }) => {
   return (
-    <div className="group relative flex sm:aspect-square h-fit flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border-2 border-amber-500 bg-white max-sm:flex-col">
-      <div
-        onClick={() => setOpen((open) => !open)}
-        style={{
-          backgroundImage: `url("${image}")`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-        className={clsx(
-          open ? "h-[25rem]" : "h-[15rem]",
-          "sm:aspect-square sm:h-full w-full overflow-hidden transition-all duration-300 ease-in-out"
-        )}
-      ></div>
-      <div
-        className={clsx(
-          "absolute flex w-full cursor-default flex-col bottom-0 bg-amber-100 py-2 font-semibold",
-          open ? "h-full" : "h-16"
-        )}
-      >
-        <span className="w-full text-center text-xl font-semibold text-black">
-          {title}
-        </span>
-        {open && (
-          <div className="flex max-h-full w-full flex-col gap-1 overflow-y-auto px-3 py-5 text-lg text-black scrollbar-none">
-            {category?.sub.map((subItem) => (
-              <Link
-                href={`${link}?${title}=${subItem.title}`}
-                className="w-fit border-b-2 border-transparent font-normal hover:underline"
-              >
-                {subItem.title}
-              </Link>
-            ))}
-          </div>
-        )}
+    <Link
+      href={link}
+      className="relative isolate flex aspect-[2/3] h-full w-full overflow-hidden rounded-lg p-2"
+    >
+      <Image
+        src={image}
+        alt={main}
+        width={200}
+        height={500}
+        className="absolute left-0 top-0 -z-10 h-full w-full rounded-md object-cover object-top"
+      />
+      <div className="mt-auto flex h-10 w-full items-center justify-center rounded-md bg-white">
+        {main}
       </div>
-
-      <UIButton
-        onClick={() => setOpen((open) => !open)}
-        className="absolute bottom-4 right-4 grid h-8 w-8 place-content-center rounded-full border-amber-500 bg-white text-black hover:outline-amber-200"
-      >
-        <FaAngleDown
-          className={`${
-            !open ? "sm:rotate-180" : "max-sm:rotate-180"
-          } transition-all ease-in-out`}
-        />
-      </UIButton>
-    </div>
+    </Link>
   );
 };
 
