d8a01971f0818bd539b3c63aa1fadc4c998a0e72
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-06 13:36:34 +0530

    ⬆️update: input ui

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -11,7 +11,7 @@ export const Input: FC<{
     <div className="relative h-10 w-full">
       <input
         type={input_type}
-        className="peer h-full w-full rounded-md border-2 border-gray-400 bg-transparent px-3 outline-none outline-offset-0 focus:border-black hover:outline hover:outline-4 hover:outline-gray-300"
+        className="peer h-full w-full rounded-md border-2 border-gray-400 bg-transparent px-3 outline-none outline-offset-0 hover:outline hover:outline-4 hover:outline-gray-300 focus:border-black"
         id={placeholder}
         value={value}
         onChange={(e) => setValue(e.target.value)}
@@ -19,8 +19,8 @@ export const Input: FC<{
       <label
         htmlFor={placeholder}
         className={cn(
-          "absolute left-3 -translate-y-1/2 bg-white px-2 text-lg transition-all delay-300 ease-in-out peer-focus:top-0 peer-focus:text-sm peer-focus:font-semibold peer-focus:text-black",
-          !!value ? "top-0 text-sm" : "top-1/2 text-lg"
+          "absolute left-3 -translate-y-1/2 bg-white px-2 text-gray-500 transition-all delay-300 ease-in-out peer-focus:top-0 peer-focus:text-xs peer-focus:font-medium peer-focus:text-black",
+          !!value ? "top-0 text-xs" : "top-1/2 text-sm"
         )}
       >
         {placeholder}
