58052fdcbb2ec16194fbbf58841b6374d412df3b
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-06 15:06:24 +0530

    ⬆️update: edit profile

--- src/app/user/details/edit/page.tsx
+++ src/app/user/details/edit/page.tsx
@@ -12,6 +12,7 @@ import {
   showNotification,
 } from "@/redux/slice/notification.slice";
 import axios from "axios";
+import z from "zod";
 
 const EditProfile: FC = () => {
   const user = useAppSelector(selectUser);
@@ -20,6 +21,38 @@ const EditProfile: FC = () => {
   const [username, setUsername] = useState(user.username);
   const [mobileNumber, setMobileNumber] = useState(user.Mobile_No?.toString());
   const [email, setEmail] = useState(user.email);
+
+  const validateTypes = (): { valid: boolean } & NotificationType => {
+    const emailSchema = z.string().email();
+    try {
+      emailSchema.parse(email);
+    } catch {
+      return {
+        valid: false,
+        message: "not a valid email",
+        type: "warning",
+        content:
+          "the email you have provide is not a valid email format an example of a valid email is: ashokkumar@email.com",
+      };
+    }
+
+    const mobileNumberSchema = z
+      .number()
+      .refine((number) => number.toString().length === 10);
+    try {
+      mobileNumberSchema.parse(Number(mobileNumber));
+    } catch {
+      return {
+        valid: false,
+        message: "not a valid mobile number",
+        type: "warning",
+        content:
+          "By Indian standards a valid mobile number must be a 10 digit number",
+      };
+    }
+
+    return { valid: true, message: "", type: "info", content: "" };
+  };
 
   const submit = async () => {
     if (!username?.trim() || !mobileNumber?.trim() || !email?.trim()) {
@@ -34,6 +67,12 @@ const EditProfile: FC = () => {
       return dispatch(showNotification());
     }
 
+    const { valid, message, type, content } = validateTypes();
+    if (!valid) {
+      dispatch(setNotification({ message, type, content }));
+      return dispatch(showNotification());
+    }
+
     await axios
       .post<NotificationType>(
         `${process.env.ENDPOINT}/api/v1/user/update`,
@@ -44,7 +83,6 @@ const EditProfile: FC = () => {
         },
         {
           headers: {
-            "Authorization": `Bearer ${user.accessToken}`,
             "Content-Type": "application/json",
           },
           withCredentials: true,
@@ -70,46 +108,44 @@ const EditProfile: FC = () => {
   };
 
   return (
-    <div className="flex w-full flex-col gap-3">
-      <Container containerTitle="Edit Profile Details">
-        <div className="mx-auto flex w-full max-w-lg flex-col gap-6">
-          <div className="flex w-full flex-col gap-2">
-            <span className="shrink-0 text-lg">Update username:</span>
-            <Input
-              input_type="text"
-              placeholder=""
-              value={username}
-              setValue={setUsername}
-            />
-          </div>
-          <div className="flex w-full flex-col gap-2">
-            <span className="shrink-0 text-lg">Update email:</span>
-            <Input
-              input_type="text"
-              placeholder=""
-              value={email}
-              setValue={setEmail}
-            />
-          </div>
-          <div className="flex w-full flex-col gap-2">
-            <span className="shrink-0 text-lg">Update mobile number:</span>
-            <Input
-              input_type="text"
-              placeholder=""
-              value={mobileNumber}
-              setValue={setMobileNumber}
-            />
-          </div>
-
-          <UIButton
-            onClick={submit}
-            className="ml-auto w-full bg-sky-100 hover:outline-sky-200"
-          >
-            SUBMIT
-          </UIButton>
+    <Container containerTitle="Edit Profile Details">
+      <div className="mx-auto flex w-full flex-col gap-6 md:max-w-lg">
+        <div className="flex w-full flex-col gap-2">
+          <span className="shrink-0 text-lg">Update username:</span>
+          <Input
+            input_type="text"
+            placeholder=""
+            value={username}
+            setValue={setUsername}
+          />
         </div>
-      </Container>
-    </div>
+        <div className="flex w-full flex-col gap-2">
+          <span className="shrink-0 text-lg">Update email:</span>
+          <Input
+            input_type="text"
+            placeholder=""
+            value={email}
+            setValue={setEmail}
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <span className="shrink-0 text-lg">Update mobile number:</span>
+          <Input
+            input_type="text"
+            placeholder=""
+            value={mobileNumber}
+            setValue={setMobileNumber}
+          />
+        </div>
+
+        <UIButton
+          onClick={submit}
+          className="ml-auto w-full bg-sky-100 hover:outline-sky-200"
+        >
+          SUBMIT
+        </UIButton>
+      </div>
+    </Container>
   );
 };
 
