d1dea445c31d7122ed2ce54826330c3aded1e4a1
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-08 19:38:36 +0530

    ➕Add: user review

---
+++ src/components/userReviewCarousel.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { FC, useEffect, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import Image from "next/image";
+import { userReviews } from "@/data/userReview";
+
+const UserReviewCarousel: FC = () => {
+  const [isMobile, setIsMobile] = useState<boolean>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="mt-10 w-full bg-white px-[5vw]">
+      <div className="flex h-[30rem] flex-col gap-2">
+        <div className="text-center font-bold">Customer Review</div>
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={20}
+          slidesPerView={isMobile ? 1 : 2}
+          className="flex w-full"
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+        >
+          {userReviews.map((review, index) => (
+            <SwiperSlide key={review.image + index}>
+              <div className="flex w-full gap-5 rounded-xl border-2 border-gray-300 p-5">
+                <Image
+                  src={review.image}
+                  alt={review.name}
+                  width={500}
+                  height={500}
+                  className="h-[20rem] w-full rounded-lg object-contain"
+                />
+                <div className="flex flex-col items-center justify-center gap-3">
+                  <i className="text-center text-sm text-gray-400">
+                    "{review.review}"
+                  </i>
+                  <div>{review.name}</div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default UserReviewCarousel;
---
+++ src/data/userReview.ts
@@ -0,0 +1,49 @@
+type Review = {
+  image: string;
+  review: string;
+  name: string;
+};
+export const userReviews: Review[] = [
+  {
+    image: "/temp/Salwar Kameez.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Indo Western.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Lehenga's.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Kurti's.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Bridal.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Western Wear.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+  {
+    image: "/temp/Dress Material's.png",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
+    name: "Customer name",
+  },
+];
