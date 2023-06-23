54b0442fcffcde4d5f853341d0a81b23ca0175fd
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-23 10:28:43 +0530

    ✨feature: product filteration

--- src/app/(product)/category/[categoryName]/page.tsx
+++ src/app/(product)/category/[categoryName]/page.tsx
@@ -1,12 +1,47 @@
 "use client";
 
-import ProductCard from "@/components/productCard";
+import ProductCard, { DummyProductCard } from "@/components/productCard";
 import FilterBar from "@/components/sidebars/filterBar/filterBar";
-import { useParams } from "next/navigation";
+import { FC, useState, useEffect } from "react";
+import { useParams, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { ProductType } from "@/components/header/header";
 
-const CategoryPage = async () => {
+const CategoryPage: FC = () => {
   const params = useParams();
   const categoryName = decodeURIComponent(params["categoryName"]);
+  const searchParams = useSearchParams();
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    const current = new URLSearchParams(searchParams.toString());
+
+    current.delete(categoryName);
+    const query = `?MainCategory=${categoryName}&page=0${
+      !!current.toString() ? `&${current.toString()}` : ""
+    }${
+      !!searchParams.get(categoryName)
+        ? `&SubCategory=${encodeURIComponent(searchParams.get(categoryName)!)}`
+        : ""
+    }`;
+
+    axios
+      .get<{
+        totalPages: number;
+        currentPage: number;
+        products: ProductType[];
+      }>(
+        `${process.env.ENDPOINT}/api/v1/user/getallProductsFromFilters${query}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        setProducts(res.data.products);
+      });
+  }, [categoryName, searchParams]);
 
   return (
     <div className="mb-10 border-b-2 border-gray-300 pt-[93px] max-md:pt-28">
@@ -15,7 +50,6 @@ const CategoryPage = async () => {
           {categoryName}
         </span>
       </div>
-
       <div className="flex">
         <FilterBar />
         <div className="flex w-full flex-col gap-3 px-[3vw] pb-10 pt-5 text-justify sm:pl-4">
@@ -50,11 +84,14 @@ const CategoryPage = async () => {
             </span>
           </div>
 
-          <div className="grid grid-cols-2 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {Array(12)
+          <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            {products.map((product) => (
+              <ProductCard key={product.name} product={product} />
+            ))}
+            {Array(30)
               .fill(null)
               .map((_, index) => (
-                <ProductCard key={index} />
+                <DummyProductCard key={index} />
               ))}
           </div>
         </div>
