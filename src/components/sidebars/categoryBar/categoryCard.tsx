4dd6f3e97ca2221711d81fc44fdce581c06fd3a9
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-27 14:06:00 +0530

    ➕Add: category cards

--- src/components/sidebars/categoryBar/categoryCard.tsx
+++ src/components/sidebars/categoryBar/categoryCard.tsx
@@ -1,27 +1,23 @@
 import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { CategoryType } from "@/redux/slice/category.slice";
 
-interface Props {
-  image: string;
-  main: string;
-  link: string;
-}
-const CategoryCard: FC<Props> = ({ main, image, link }) => {
+const CategoryCard: FC<CategoryType> = (category) => {
   return (
     <Link
-      href={link}
+      href={`/category/${category.Title}`}
       className="relative isolate flex aspect-[2/3] h-full w-full overflow-hidden rounded-lg p-2"
     >
       <Image
-        src={image}
-        alt={main}
+        src={category.Image}
+        alt={category.Title + " image"}
         width={200}
         height={500}
         className="absolute left-0 top-0 -z-10 h-full w-full rounded-md object-cover object-top"
       />
-      <div className="mt-auto flex h-10 w-full items-center justify-center rounded-md bg-white bg-opacity-70">
-        {main}
+      <div className="mt-auto flex h-10 w-full items-center justify-center rounded-md bg-white bg-opacity-70 capitalize">
+        {category.Title}
       </div>
     </Link>
   );
