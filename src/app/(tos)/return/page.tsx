7778f141d5d853a78afc2775841619aa6a6980c7
Vishal-Kamath <vishalkamath853@gmail.com>
2023-06-30 11:13:02 +0530

    ➕Add: TOS pages

---
+++ src/app/(tos)/return/page.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+
+const ReturnPolicyPage: NextPage = () => {
+  return (
+    <div className="flex flex-col gap-5 px-[3vw] pb-10 pt-[93px] max-md:pt-28">
+      <h1 className="flex items-baseline gap-4 border-b-2 border-gray-300 py-5">
+        <span className="text-xl font-bold">RETURN POLICY</span>
+      </h1>
+      <div className="text-md text-justify">
+        <div className="font-semibold">Sanskruti nx</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, magni
+        culpa officia beatae rem non qui quod illo amet consequuntur vero ipsum
+        quidem nam deleniti voluptatum eaque harum voluptas est. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Commodi eius quasi neque,
+        facilis laboriosam, saepe molestias repellendus facere tempore illo
+        culpa deleniti in distinctio asperiores. Cum repellat laboriosam ipsa
+        eligendi? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Maxime, ipsam eaque? Nesciunt deleniti ipsum dolor, praesentium quos
+        quia unde, accusamus asperiores aliquam quam ipsa, accusantium dolorum
+        consectetur vero debitis officiis.
+      </div>
+    </div>
+  );
+};
+
+export default ReturnPolicyPage;
