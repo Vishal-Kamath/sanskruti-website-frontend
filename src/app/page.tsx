8b711dfaf05e4a1d0e048c0a868fbbadd03c7cc1
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-03 21:56:07 +0530

    ➕Add: tos icons ⬆️update: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,66 @@
 import ProductCarousel from "@/components/common/productCarousel";
+import VisitOurStore from "@/components/footer/visitOurStore";
 import Carousel from "@/components/mainCarousel";
 import CategoryBar from "@/components/sidebars/categoryBar/categoryBar";
 import UserReviewCarousel from "@/components/userReviewCarousel";
 import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col gap-5 pb-10 pt-[93px] max-md:pt-28">
       <Carousel />
+
+      <div className="flex justify-evenly py-5">
+        <Link href="#" className="flex h-full flex-col items-center gap-2">
+          <Image
+            className="h-8 w-8 object-center lg:h-10 lg:w-10"
+            src="/assets/shippingTruck.svg"
+            alt="shipping truck"
+            width={50}
+            height={50}
+          />
+          <div className="max-lg:text-xs">Free Shipping</div>
+        </Link>
+        <Link href="#" className="flex h-full flex-col items-center gap-2">
+          <Image
+            className="h-8 w-8 object-center lg:h-10 lg:w-10"
+            src="/assets/customFitting.svg"
+            alt="shipping truck"
+            width={50}
+            height={50}
+          />
+          <div className="max-lg:text-xs">Easy Return</div>
+        </Link>
+        <Link href="#" className="flex h-full flex-col items-center gap-2">
+          <Image
+            className="h-8 w-8 object-center lg:h-10 lg:w-10"
+            src="/assets/easyReturn.svg"
+            alt="easy return"
+            width={50}
+            height={50}
+          />
+          <div className="max-lg:text-xs">Custom Fitting</div>
+        </Link>
+      </div>
+
       <CategoryBar />
       <ProductCarousel />
       <ProductCarousel />
       <UserReviewCarousel />
+
+      <a href="" className="px-[3vw]" target="_blank">
+        <Image
+          src="/assets/footerInstagramImage.png"
+          alt="Visit our instagram diaries"
+          width={600}
+          height={600}
+          className="w-full"
+        />
+      </a>
+
+      <VisitOurStore />
     </main>
   );
 };
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,22 +1,9 @@
 import { FC } from "react";
 import FooterDetails from "./footerDetails";
-import Image from "next/image";
-import VisitOurStore from "./visitOurStore";
 
 const Footer: FC = () => {
   return (
     <footer className="mt-auto flex flex-col gap-3">
-      <a href="" className="px-[3vw]" target="_blank">
-        <Image
-          src="/assets/footerInstagramImage.png"
-          alt="Visit our instagram diaries"
-          width={600}
-          height={600}
-          className="w-full"
-        />
-      </a>
-
-      <VisitOurStore />
       <FooterDetails />
     </footer>
   );
