0446ff3b17d820ca1b95fc6dcfa6449986dfdfc4
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-06 16:01:43 +0530

    ⬆️update: category ui

--- src/components/sidebars/categoryBar/categoryCard.tsx
+++ src/components/sidebars/categoryBar/categoryCard.tsx
@@ -7,16 +7,16 @@ const CategoryCard: FC<CategoryType> = (category) => {
   return (
     <Link
       href={`/category/${category.Title}`}
-      className="relative isolate flex aspect-[2/3] h-full w-full overflow-hidden rounded-lg p-2"
+      className="relative isolate flex aspect-[2/3] h-full w-full overflow-hidden rounded-sm p-2"
     >
       <Image
         src={category.Image}
         alt={category.Title + " image"}
         width={200}
         height={500}
-        className="absolute left-0 top-0 -z-10 h-full w-full rounded-md object-cover object-top"
+        className="absolute left-0 top-0 -z-10 h-full w-full rounded-sm object-cover object-top"
       />
-      <div className="mt-auto flex h-10 w-full items-center justify-center rounded-md bg-white bg-opacity-70 capitalize">
+      <div className="mt-auto flex h-10 w-full items-center justify-center rounded-sm bg-white bg-opacity-70 capitalize">
         {category.Title}
       </div>
     </Link>
