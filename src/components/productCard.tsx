a222a689f9b68695a51c85f083d3d496b1308822
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-06 20:26:46 +0530

    ⬆️update: product like

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -80,60 +80,61 @@ const ProductCard: FC<Props> = ({ className, product }) => {
     : product.varients.variations[0]?.price;
 
   return (
-    <div className={cn("flex w-full flex-shrink-0 flex-col gap-2", className)}>
-      <Link href={`/product/${product.slug}`}>
-        <div className="bg-gray-100">
-          {product.images.length !== 0 ? (
-            <Image
-              src={product.images[0]}
-              width={150}
-              height={150}
-              className="aspect-[2/2.5] h-full w-full object-cover object-top"
-              alt="Product image"
-            />
-          ) : null}
+    <div className={cn("relative w-full flex-shrink-0", className)}>
+      <Link
+        href={`/product/${product.slug}`}
+        className={cn("flex w-full flex-shrink-0 flex-col gap-2", className)}
+      >
+        <div>
+          <div className="bg-gray-100">
+            {product.images.length !== 0 ? (
+              <Image
+                src={product.images[0]}
+                width={150}
+                height={150}
+                className="aspect-[2/2.5] h-full w-full object-cover object-top"
+                alt="Product image"
+              />
+            ) : null}
+          </div>
+        </div>
+
+        <div className="flex h-full flex-col gap-2">
+          <div className="text-[0.8rem] font-medium text-gray-500">
+            {product.name}
+          </div>
+          <div className="mt-auto flex items-center gap-2 text-sm font-[550]">
+            {!!product.varients.variations[0]?.discount ? (
+              <div className="flex items-baseline gap-2">
+                <span>&#8377;{price}</span>
+                <s className="text-gray-500">
+                  &#8377;{product.varients.variations[0]?.price}
+                </s>
+                <span className="text-red-800">
+                  ({product.varients.variations[0].discount}% OFF)
+                </span>
+              </div>
+            ) : (
+              <span>&#8377;{price}</span>
+            )}
+          </div>
         </div>
       </Link>
       <button
         onClick={handleLike}
         className={cn(
-          "flex h-8 w-full flex-shrink-0 items-center justify-center gap-2 rounded-sm border-[1px] text-xs font-medium",
+          "absolute right-0 top-0 rounded-bl-md bg-opacity-75 p-2",
           liked
-            ? "border-red-300 bg-red-50 text-red-500 hover:border-red-600"
-            : "border-gray-300 text-gray-500 hover:border-gray-600 hover:text-black"
+            ? "border-red-300 bg-red-200 text-red-500 hover:border-red-600"
+            : "border-gray-300 bg-white text-black hover:border-gray-600 hover:text-black"
         )}
       >
-        <span>WISHLIST</span>
         {liked ? (
           <AiFillHeart className="h-4 w-4 text-red-600" />
         ) : (
           <AiOutlineHeart className="h-4 w-4" />
         )}
       </button>
-
-      <Link
-        href={`/product/${product.slug}`}
-        className="flex h-full flex-col gap-2"
-      >
-        <div className="text-[0.8rem] font-medium text-gray-500">
-          {product.name}
-        </div>
-        <div className="mt-auto flex items-center gap-2 text-sm font-[550]">
-          {!!product.varients.variations[0]?.discount ? (
-            <div className="flex items-baseline gap-2">
-              <span>&#8377;{price}</span>
-              <s className="text-gray-500">
-                &#8377;{product.varients.variations[0]?.price}
-              </s>
-              <span className="text-red-800">
-                ({product.varients.variations[0].discount}% OFF)
-              </span>
-            </div>
-          ) : (
-            <span>&#8377;{price}</span>
-          )}
-        </div>
-      </Link>
     </div>
   );
 };
@@ -143,45 +144,48 @@ interface DummyProps extends HTMLAttributes<HTMLDivElement> {}
 export const DummyProductCard: FC<DummyProps> = ({ className }) => {
   const [liked, setLiked] = useState(false);
   return (
-    <div className={cn("flex w-full flex-shrink-0 flex-col gap-2", className)}>
-      <div>
-        <div className="bg-gray-100">
-          <Image
-            src={filters[5].image}
-            width={150}
-            height={150}
-            className="aspect-[2/2.5] h-full w-full object-cover object-top"
-            alt="Product image"
-          />
+    <div className={cn("relative w-full flex-shrink-0", className)}>
+      <div
+        className={cn("flex w-full flex-shrink-0 flex-col gap-2", className)}
+      >
+        <div>
+          <div className="bg-gray-100">
+            <Image
+              src={filters[5].image}
+              width={150}
+              height={150}
+              className="aspect-[2/2.5] h-full w-full object-cover object-top"
+              alt="Product image"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <div className="text-[0.8rem] font-medium text-gray-500">
+            Mint Green Printed Palazzo Suit In Chanderi With Embroidery Mint
+            Green Printed
+          </div>
+          <div className="flex items-center gap-2 text-sm font-[550]">
+            <span>&#8377;4,000</span>
+            <s className="text-gray-500">&#8377;4,440</s>
+          </div>
         </div>
       </div>
       <button
         onClick={() => setLiked((like) => !like)}
         className={cn(
-          "flex h-8 w-full flex-shrink-0 items-center justify-center gap-2 rounded-sm border-[1px] text-xs font-medium",
+          "absolute right-0 top-0 rounded-bl-md bg-opacity-75 p-2",
           liked
-            ? "border-red-300 bg-red-50 text-red-500 hover:border-red-600"
-            : "border-gray-300 text-gray-500 hover:border-gray-600 hover:text-black"
+            ? "border-red-300 bg-red-200 text-red-500 hover:border-red-600"
+            : "border-gray-300 bg-white text-black hover:border-gray-600 hover:text-black"
         )}
       >
-        <span>WISHLIST</span>
         {liked ? (
           <AiFillHeart className="h-4 w-4 text-red-600" />
         ) : (
           <AiOutlineHeart className="h-4 w-4" />
         )}
       </button>
-
-      <div className="flex flex-col gap-2">
-        <div className="text-[0.8rem] font-medium text-gray-500">
-          Mint Green Printed Palazzo Suit In Chanderi With Embroidery Mint Green
-          Printed
-        </div>
-        <div className="flex items-center gap-2 text-sm font-[550]">
-          <span>&#8377;4,000</span>
-          <s className="text-gray-500">&#8377;4,440</s>
-        </div>
-      </div>
     </div>
   );
 };
