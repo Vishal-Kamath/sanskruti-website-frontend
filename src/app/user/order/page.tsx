128c57b0a270b0521f190d0ca70eb0336e863854
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-07 20:38:13 +0530

    ➕Add: search orders

--- src/app/user/order/page.tsx
+++ src/app/user/order/page.tsx
@@ -8,6 +8,7 @@ import Container from "../components/container";
 import OrderComponet from "../components/orderComponent";
 import { useAppDispatch } from "@/redux/store/hooks";
 import { completeLoading, startLoading } from "@/redux/slice/loading.slice";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export type Order = {
   order: {
@@ -72,6 +73,17 @@ const OrderHistoryPage: NextPage = () => {
   const [orders, setOrders] = useState<Order[]>();
   const dispatch = useAppDispatch();
 
+  const [search, setSearch] = useState("");
+
+  const orderList =
+    (orders &&
+      orders.filter((order) =>
+        order.order.product.name
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase())
+      )) ||
+    [];
+
   const getOrders = () => {
     dispatch(startLoading());
     axios
@@ -99,11 +111,26 @@ const OrderHistoryPage: NextPage = () => {
 
   return (
     <Container containerTitle="Order History">
-      <div className="flex flex-col gap-2 pt-5">
-        {orders &&
-          orders.map((order) => (
+      <div className="flex flex-col gap-2 pt-1">
+        <div className="text-md flex h-9 w-full items-center gap-1 rounded-md border-2 border-gray-300 bg-slate-50 px-2 text-gray-400 focus-within:border-gray-600 focus-within:text-gray-600">
+          <AiOutlineSearch className="aspect-sqaure text-xl" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            className="w-full border-none bg-transparent outline-none"
+            placeholder="Search for past orders"
+          />
+        </div>
+        {orderList.length ? (
+          orderList.map((order) => (
             <OrderComponet key={order.payment.orderId} order={order} />
-          ))}
+          ))
+        ) : (
+          <div className="mt-5 text-center text-lg font-semibold">
+            No orders Found
+          </div>
+        )}
       </div>
     </Container>
   );
