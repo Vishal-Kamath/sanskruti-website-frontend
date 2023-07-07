e10199fbc38bbddf927734d3f8f3ce3160357edc
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-07 23:33:57 +0530

    ⬆️update: order component responsive

--- src/app/user/components/orderComponent.tsx
+++ src/app/user/components/orderComponent.tsx
@@ -18,9 +18,9 @@ const OrderComponet: FC<{ order: Order }> = ({ order }) => {
   return (
     <Link
       href={`/user/order/${order.order._id}`}
-      className="flex items-start justify-between gap-2 rounded-md border-[1px] border-gray-300 p-4"
+      className="flex items-start justify-between gap-2 rounded-md border-[1px] border-gray-300 p-4 max-md:flex-col"
     >
-      <div className="flex items-start gap-2">
+      <div className="flex w-full items-start gap-2">
         <Image
           src={order.order.product.images[0]}
           key={order.order.product.id + order.order.orderId}
@@ -29,51 +29,54 @@ const OrderComponet: FC<{ order: Order }> = ({ order }) => {
           height={50}
           className="h-[8rem] w-[10rem] overflow-hidden object-contain object-center"
         />
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-1">
-            <h3 className="text-[14px] font-semibold">
-              {order.order.product.name.length > 30
-                ? `${order.order.product.name.slice(0, 30)}...`
-                : order.order.product.name}
-            </h3>
-            <span className="text-xs font-bold text-gray-500">
-              {order.order.product.brand_name}
-            </span>
-          </div>
-
-          <div className="flex flex-wrap gap-2 text-xs">
-            {variations.map((val) => (
-              <span
-                key={val + order.order._id}
-                className="rounded-full border-[1px] border-gray-400 bg-slate-50 px-2 py-1"
-              >
-                {val}
+        <div className="flex w-full items-start justify-between gap-2 max-lg:flex-col">
+          <div className="flex w-full flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <h3 className="text-[14px] font-semibold">
+                {order.order.product.name.length > 30
+                  ? `${order.order.product.name.slice(0, 30)}...`
+                  : order.order.product.name}
+              </h3>
+              <span className="text-xs font-bold text-gray-500">
+                {order.order.product.brand_name}
               </span>
-            ))}
-            <span className="rounded-full border-[1px] border-gray-400 bg-slate-50 px-2 py-1">
-              quantity: {order.order.product.quantity}
-            </span>
+            </div>
+
+            <div className="flex flex-wrap gap-2 text-xs">
+              {variations.map((val) => (
+                <span
+                  key={val + order.order._id}
+                  className="rounded-full border-[1px] border-gray-400 bg-slate-50 px-2 py-1"
+                >
+                  {val}
+                </span>
+              ))}
+              <span className="rounded-full border-[1px] border-gray-400 bg-slate-50 px-2 py-1">
+                quantity: {order.order.product.quantity}
+              </span>
+            </div>
+          </div>
+          <div className="flex w-full gap-1 text-[14px] font-semibold lg:justify-center">
+            {!!order.order.product.varient.discount ? (
+              <div className="flex items-baseline gap-2">
+                <span>&#8377;{price}</span>
+                <s className="text-gray-500">
+                  &#8377;
+                  {order.order.product.varient.price *
+                    order.order.product.quantity}
+                </s>
+                <span className="text-red-800">
+                  ({order.order.product.varient.discount}% OFF)
+                </span>
+              </div>
+            ) : (
+              <span>&#8377;{price}</span>
+            )}
           </div>
         </div>
       </div>
 
-      <div className="flex items-center gap-1 text-[14px] font-semibold">
-        {!!order.order.product.varient.discount ? (
-          <div className="flex items-baseline gap-2">
-            <span>&#8377;{price}</span>
-            <s className="text-gray-500">
-              &#8377;
-              {order.order.product.varient.price * order.order.product.quantity}
-            </s>
-            <span className="text-red-800">
-              ({order.order.product.varient.discount}% OFF)
-            </span>
-          </div>
-        ) : (
-          <span>&#8377;{price}</span>
-        )}
-      </div>
-      <div className="flex flex-col gap-1 text-right">
+      <div className="flex flex-shrink-0 gap-2 text-right max-md:w-full max-md:items-baseline max-md:justify-center md:flex-col">
         <div className="flex items-center gap-2 text-[14px] font-semibold">
           {order.order.deliveryInfo.status === "Delivered" ? (
             <BsFillCheckCircleFill className="h-3 w-3 text-sanskrutiRed" />
