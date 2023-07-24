93cc5858ed6262acbd5bdef216dd0d592b267d8b
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-24 10:57:44 +0530

    ⬆️update: user review

--- src/components/userReviewCarousel.tsx
+++ src/components/userReviewCarousel.tsx
@@ -2,20 +2,29 @@
 
 import { FC, useEffect, useState } from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
 
 import "swiper/css";
 import "swiper/css/autoplay";
+
 import Image from "next/image";
 import { userReviews } from "@/data/userReview";
+import UIHeader from "./common/header";
+import SwiperContainer from "./common/swiperContainer";
 
 const UserReviewCarousel: FC = () => {
-  const [isMobile, setIsMobile] = useState<boolean>();
+  const [slideNum, setSlideNum] = useState(3);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 1024);
+      if (window.innerWidth > 1024) {
+        setSlideNum(3);
+      } else if (window.innerWidth > 768) {
+        setSlideNum(2);
+      } else {
+        setSlideNum(1);
+      }
     };
 
     handleResize();
@@ -27,13 +36,13 @@ const UserReviewCarousel: FC = () => {
   }, []);
 
   return (
-    <div className="mt-10 w-full bg-white px-[3vw]">
-      <div className="flex h-[35rem] flex-col gap-2 sm:h-[30rem]">
-        <div className="text-center text-lg font-bold">Customer Review</div>
-        <Swiper
+    <div className="my-10 w-full bg-white px-[3vw]">
+      <div className="flex flex-col gap-2">
+        <UIHeader title="Customer Review's" />
+        <SwiperContainer
           modules={[Autoplay]}
           spaceBetween={20}
-          slidesPerView={isMobile ? 1 : 2}
+          slidesPerView={slideNum}
           className="flex w-full"
           autoplay={{
             delay: 5000,
@@ -42,24 +51,26 @@ const UserReviewCarousel: FC = () => {
         >
           {userReviews.map((review, index) => (
             <SwiperSlide key={review.image + index}>
-              <div className="flex w-full items-center gap-5 rounded-xl border-2 border-gray-300 p-5 max-sm:flex-col">
+              <div className="flex w-full flex-col items-center gap-5 overflow-hidden rounded-xl border-[1px] border-gray-400 px-5 py-12">
                 <Image
                   src={review.image}
                   alt={review.name}
                   width={500}
                   height={500}
-                  className="h-[20rem] w-fit rounded-lg object-contain"
+                  className="h-[5rem] w-[5rem] rounded-full object-cover object-top"
                 />
-                <div className="flex flex-col items-center justify-center gap-3">
-                  <i className="text-center text-sm text-gray-400">
+                <div className="flex w-full flex-col items-center justify-center gap-5">
+                  <i className="text-center text-lg text-gray-700">
                     &quot;{review.review}&quot;
                   </i>
-                  <div>{review.name}</div>
+                  <div className="border-t-4 border-slate-400 pt-5 text-[16px]">
+                    {review.name}
+                  </div>
                 </div>
               </div>
             </SwiperSlide>
           ))}
-        </Swiper>
+        </SwiperContainer>
       </div>
     </div>
   );
