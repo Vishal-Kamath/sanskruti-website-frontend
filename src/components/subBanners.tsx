75de9f73afc8f1677b543a138758769ac5f10536
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-24 10:57:04 +0530

    ➕Add: editors pick

--- src/components/subBanners.tsx
+++ src/components/subBanners.tsx
@@ -11,6 +11,7 @@ import axios from "axios";
 import SwiperContainer from "./common/swiperContainer";
 import { useAppDispatch } from "@/redux/store/hooks";
 import { completeLoading, startLoading } from "@/redux/slice/loading.slice";
+import UIHeader from "./common/header";
 
 type Banner = {
   isPublished: boolean;
@@ -57,32 +58,35 @@ const SubBanner: FC = () => {
   }, []);
 
   return (
-    <div className="h-fit px-[3vw]">
-      <SwiperContainer
-        modules={[Autoplay]}
-        loop={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        slidesPerView={2}
-        spaceBetween={10}
-      >
-        {banners?.map((banner, index) => {
-          const val = isMobile ? "mobileImage" : "desktopImage";
-          return (
-            <SwiperSlide className="-z-10" key={"sub banner slide " + index}>
-              <Image
-                src={banner[val]}
-                alt={"sub banner image" + index}
-                width={500}
-                height={500}
-                className="h-full w-full object-cover max-md:object-top xl:max-h-[70vh]"
-              />
-            </SwiperSlide>
-          );
-        })}
-      </SwiperContainer>
+    <div className="flex flex-col gap-5 px-[3vw] py-5">
+      <UIHeader title="Editor's Pick" />
+      <div className="h-fit">
+        <SwiperContainer
+          modules={[Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          slidesPerView={2}
+          spaceBetween={10}
+        >
+          {banners?.map((banner, index) => {
+            const val = isMobile ? "mobileImage" : "desktopImage";
+            return (
+              <SwiperSlide className="-z-10" key={"sub banner slide " + index}>
+                <Image
+                  src={banner[val]}
+                  alt={"sub banner image" + index}
+                  width={500}
+                  height={500}
+                  className="h-full w-full object-cover max-md:object-top xl:max-h-[70vh]"
+                />
+              </SwiperSlide>
+            );
+          })}
+        </SwiperContainer>
+      </div>
     </div>
   );
 };
