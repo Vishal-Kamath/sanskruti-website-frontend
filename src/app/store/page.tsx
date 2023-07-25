2c1513c9461151aa6bcd16da03ee05b450f6afc1
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-25 18:10:01 +0530

    ➕Add: store page

---
+++ src/app/store/page.tsx
@@ -0,0 +1,48 @@
+import UIButton from "@/components/common/button";
+import UIGoogleMap from "@/components/common/map";
+import UserReviewCarousel from "@/components/userReviewCarousel";
+import Image from "next/image";
+import { FC } from "react";
+
+const StorePage: FC = () => {
+  return (
+    <div className="flex min-h-screen flex-col gap-5 pb-10 pt-36">
+      <div className="relative isolate mb-[20rem] w-full sm:mb-[6rem] sm:px-[3vw]">
+        <Image
+          src="/assets/storeImage.png"
+          alt="store image"
+          width={1500}
+          height={1500}
+          className="h-[40vh] w-full object-cover sm:h-[60vh] sm:rounded-b-xl"
+        />
+        <div className="absolute bottom-0 left-1/2 w-full -translate-x-1/2 translate-y-[20rem] px-[3vw] sm:translate-y-[5rem]">
+          <div className="mx-auto flex w-full max-w-4xl justify-between gap-4 rounded-md border-[1px] border-slate-300 bg-white p-5 max-sm:flex-col">
+            <div className="flex flex-col gap-3 text-[16px] sm:text-lg">
+              <h3 className="text-lg font-bold sm:text-xl">
+                Sanskruti Nx at Kalyan
+              </h3>
+              <div className="max-w-xs">
+                Shop No. 2, 3, 4, Yashoda Vinayak Sankul, Agra Rd, Opposite
+                Suchak Petrol Pump, Kalyan(West), Maharashtra 421301
+              </div>
+              <a
+                className="mt-auto"
+                target="_blank"
+                href="https://goo.gl/maps/LaGnDHJ6WhTNJDsGA"
+              >
+                <UIButton className="w-fit px-3">Get Directions</UIButton>
+              </a>
+            </div>
+            <UIGoogleMap
+              className="aspect-square w-full max-w-[20rem] max-sm:mx-auto sm:h-[15rem] sm:w-[15rem]"
+              src="https://maps.google.com/maps?q=Sanskruti+-+Kalyan&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+            />
+          </div>
+        </div>
+      </div>
+      <UserReviewCarousel />
+    </div>
+  );
+};
+
+export default StorePage;
---
+++ src/components/common/map.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { cn } from "@/utils/lib";
+import { FC } from "react";
+import "./css/map.css";
+import { FaSpinner } from "react-icons/fa";
+
+interface Props {
+  className: string;
+  src: string;
+}
+const UIGoogleMap: FC<Props> = ({ className, src }) => {
+  try {
+    return (
+      <div
+        className={cn("relative isolate overflow-hidden rounded-sm", className)}
+      >
+        <iframe
+          src={src}
+          className="h-full w-full"
+          sandbox="allow-scripts"
+        ></iframe>
+        <div className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-center bg-slate-100">
+          <FaSpinner className="spinner h-10 w-10 text-slate-500" />
+        </div>
+      </div>
+    );
+  } catch (err) {
+    console.log("map error ", err);
+    return (
+      <div
+        className={cn(
+          "flex items-center justify-center rounded-sm border-[1px] border-red-500 bg-red-100 text-red-500",
+          className
+        )}
+      >
+        <span>Opps!! something went wrong</span>
+      </div>
+    );
+  }
+};
+
+export default UIGoogleMap;
