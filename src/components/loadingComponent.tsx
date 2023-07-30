403839a9b3c5d3f8f0e051f914df3042e482b31c
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-30 13:34:47 +0530

    ⬆️update: loading animation

--- src/components/loadingComponent.tsx
+++ src/components/loadingComponent.tsx
@@ -4,15 +4,25 @@ import { selectLoadingState } from "@/redux/slice/loading.slice";
 import { useAppSelector } from "@/redux/store/hooks";
 import { cn } from "@/utils/lib";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 const LoadingComponent: FC = () => {
   const loading = useAppSelector(selectLoadingState);
+
+  const [loadingState, setLoadingState] = useState(true);
+  useEffect(() => {
+    if (loading.loading) {
+      setLoadingState(true);
+    } else {
+      setTimeout(() => setLoadingState(false), 200);
+    }
+  }, [loading.loading]);
+
   return (
     <div
       className={cn(
         "fixed left-0 top-0 flex min-h-screen w-full flex-col items-center justify-evenly gap-7 bg-white",
-        !loading.loading ? "hidden" : "z-[999]"
+        loading.loading || loadingState ? "z-[999]" : "hidden"
       )}
     >
       <div className="relative isolate h-fit w-fit">
@@ -26,7 +36,11 @@ const LoadingComponent: FC = () => {
         <div
           style={{
             width: `${
-              loading.total ? (loading.complete / loading.total) * 100 : 0
+              loading.loading
+                ? loading.total
+                  ? (loading.complete / loading.total) * 100
+                  : 0
+                : 100
             }%`,
           }}
           className="absolute left-0 top-0 -z-10 h-full bg-sanskrutiRed"
