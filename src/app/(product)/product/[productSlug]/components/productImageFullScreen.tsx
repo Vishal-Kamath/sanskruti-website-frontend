2e794b8017a478f403c20baf882e0afb16727506
Vishal-Kamath <vishalkamath853@gmail.com>
2023-07-30 22:27:36 +0530

    ⬆️update: full screen ui

--- src/app/(product)/product/[productSlug]/components/productImageFullScreen.tsx
+++ src/app/(product)/product/[productSlug]/components/productImageFullScreen.tsx
@@ -28,54 +28,64 @@ const ProductImageFullScreen: React.FC<{
   return (
     <div
       className={cn(
-        "fixed left-0 top-0 isolate z-50 grid max-h-screen min-h-screen w-full place-content-center bg-black bg-opacity-75",
+        "fixed left-0 top-0 z-50 grid h-full max-h-screen min-h-screen w-full place-content-center",
         className
       )}
     >
-      <div className="flex h-full max-h-screen w-full flex-col items-center justify-center gap-3 py-4">
-        <ImageZoom src={image} alt="product image" />
+      <div className="relative isolate h-screen w-screen bg-slate-950">
+        {/* Background Image */}
         <Image
-          width={500}
-          height={500}
+          width={50}
+          height={50}
           src={image}
-          alt="product image"
-          className="h-full object-contain md:hidden"
+          alt="product image Background"
+          className="absolute left-0 top-0 -z-10 h-full w-full object-cover opacity-50 blur-xl"
         />
-        <div className="custom_scrollbar flex h-fit max-w-[50vw] gap-2 overflow-x-auto overflow-y-hidden max-md:px-[3vw]">
-          {images.map((imageSrc, index) => (
-            <Image
-              src={imageSrc}
-              alt={index + " product image"}
-              width={50}
-              height={50}
-              key={index}
-              className={cn(
-                "aspect-auto h-20 w-auto",
-                imageIndex !== index && "opacity-60"
-              )}
-              onClick={() => handleSet(index)}
-            />
-          ))}
+        <div className="flex h-full max-h-screen w-full flex-col items-center justify-center gap-3 py-4">
+          <ImageZoom src={image} alt="product image" />
+          <Image
+            width={500}
+            height={500}
+            src={image}
+            alt="product image"
+            className="h-full object-contain md:hidden"
+          />
+          <div className="custom_scrollbar flex h-fit max-w-[50vw] gap-2 overflow-x-auto overflow-y-hidden max-md:px-[3vw]">
+            {images.map((imageSrc, index) => (
+              <Image
+                src={imageSrc}
+                alt={index + " product image"}
+                width={50}
+                height={50}
+                key={index}
+                className={cn(
+                  "aspect-auto h-20 w-auto",
+                  imageIndex !== index && "opacity-60"
+                )}
+                onClick={() => handleSet(index)}
+              />
+            ))}
+          </div>
         </div>
+        <button
+          onClick={handlePrev}
+          className="absolute left-0 top-1/2 z-50 -translate-y-1/2 rounded-r-full bg-white py-3 pl-1 pr-3 opacity-50 hover:opacity-75"
+        >
+          <FaAngleLeft className="h-6 w-6" />
+        </button>
+        <button
+          onClick={handleNext}
+          className="absolute right-0 top-1/2 z-50 -translate-y-1/2 rounded-l-full bg-white py-3 pl-3 pr-1 opacity-50 hover:opacity-75"
+        >
+          <FaAngleRight className="h-6 w-6" />
+        </button>
+        <button
+          onClick={() => setFullscreenImageOpen(false)}
+          className="absolute right-5 top-5 z-10 rounded-full bg-white bg-opacity-50 p-3 text-2xl text-slate-700 hover:bg-opacity-100"
+        >
+          <RxCross1 />
+        </button>
       </div>
-      <button
-        onClick={handlePrev}
-        className="absolute left-0 top-1/2 z-50 -translate-y-1/2 rounded-r-full bg-white py-3 pl-1 pr-3 opacity-50 hover:opacity-75"
-      >
-        <FaAngleLeft className="h-6 w-6" />
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute right-0 top-1/2 z-50 -translate-y-1/2 rounded-l-full bg-white py-3 pl-3 pr-1 opacity-50 hover:opacity-75"
-      >
-        <FaAngleRight className="h-6 w-6" />
-      </button>
-      <button
-        onClick={() => setFullscreenImageOpen(false)}
-        className="absolute right-5 top-5 z-10 border-2 border-gray-500 bg-slate-700 p-2 text-2xl text-white"
-      >
-        <RxCross1 />
-      </button>
     </div>
   );
 };
