bb0f66cadd6460b9cfea6875aa951e8f2fba4082
Vishal-Kamath <vishalkamath853@gmail.com>
2023-08-04 18:43:09 +0530

    ➕Add: loading to product

--- src/app/(product)/product/[productSlug]/page.tsx
+++ src/app/(product)/product/[productSlug]/page.tsx
@@ -5,14 +5,18 @@ import axios from "axios";
 import ProductPageComponent from "./components/productPage";
 import { FC, useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import { useAppDispatch } from "@/redux/store/hooks";
+import { completeLoading, startLoading } from "@/redux/slice/loading.slice";
 
 const ProductPage: FC = () => {
+  const dispatch = useAppDispatch();
   const params = useParams();
   const productSlug = params["productSlug"];
 
   const [product, setProduct] = useState<ProductType>();
 
   const getProduct = async (slug: string) => {
+    dispatch(startLoading());
     const newProduct = (
       await axios.get<{ productAlreadyExists: ProductType }>(
         `${process.env.ENDPOINT}/api/v1/user/product?slug=${slug}`,
@@ -24,6 +28,7 @@ const ProductPage: FC = () => {
       )
     ).data.productAlreadyExists;
     setProduct(newProduct);
+    dispatch(completeLoading());
   };
   useEffect(() => {
     getProduct(productSlug);
