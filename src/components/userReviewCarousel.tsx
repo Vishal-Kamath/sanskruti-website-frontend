807cdbacf508f4a092649c584343786f5d126013
Vishal-Kamath <vishalkamath853@gmail.com>
2023-11-29 15:45:50 +0530

    ⬆️update: user review

--- src/components/userReviewCarousel.tsx
+++ src/components/userReviewCarousel.tsx
@@ -8,11 +8,11 @@ import { Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/autoplay";
 
-import Image from "next/image";
 import { userReviews } from "@/data/userReview";
 import UIHeader from "./common/header";
 import SwiperContainer from "./common/swiperContainer";
 import { FcGoogle } from "react-icons/fc";
+import { cn } from "@/utils/lib";
 
 const UserReviewCarousel: FC = () => {
   const [slideNum, setSlideNum] = useState(3);
@@ -36,6 +36,12 @@ const UserReviewCarousel: FC = () => {
     };
   }, []);
 
+  const colorList = [
+    "bg-amber-100 text-red-700",
+    "bg-sky-100 text-blue-700",
+    "bg-purple-100 text-violet-700",
+  ];
+
   return (
     <div className="my-10 w-full bg-white px-[3vw]">
       <div className="flex flex-col gap-2">
@@ -50,29 +56,39 @@ const UserReviewCarousel: FC = () => {
             disableOnInteraction: true,
           }}
         >
-          {userReviews.map((review, index) => (
-            <SwiperSlide key={review.image + index}>
-              <div className="flex w-full flex-col items-center gap-7 overflow-hidden rounded-xl border-[1px] border-gray-400 px-5 py-7">
-                <div className="flex w-full items-center gap-4">
-                  <Image
-                    src={review.image}
-                    alt={review.name}
-                    width={500}
-                    height={500}
-                    className="h-[3rem] w-[3rem] rounded-full object-cover object-top"
-                  />
-                  <div className="text-[16px] font-semibold">{review.name}</div>
-                  <FcGoogle className="ml-auto h-6 w-6" />
+          {userReviews.map((review, index) => {
+            const Initials = review.name
+              .split(" ")
+              .map((name) => name[0].toLocaleUpperCase())
+              .slice(0, 2)
+              .join("");
+            return (
+              <SwiperSlide key={review.name + index}>
+                <div className="flex h-[15rem] w-full flex-col items-center gap-7 overflow-hidden rounded-xl border-[1px] border-gray-400 px-5 py-7">
+                  <div className="flex w-full items-center gap-4">
+                    <div
+                      className={cn(
+                        "flex aspect-square h-10 w-10 items-center justify-center rounded-full text-lg",
+                        colorList[index % 3]
+                      )}
+                    >
+                      {Initials}
+                    </div>
+                    <div className="text-[16px] font-semibold">
+                      {review.name}
+                    </div>
+                    <FcGoogle className="ml-auto h-6 w-6" />
+                  </div>
+                  <div className="flex h-full w-full flex-col items-center justify-center gap-6">
+                    <p className="text-justify font-normal text-gray-700">
+                      &quot;{review.review}&quot;
+                    </p>
+                    <div className="mt-auto h-1 w-16 bg-slate-300"></div>
+                  </div>
                 </div>
-                <div className="flex w-full flex-col items-center justify-center gap-6">
-                  <p className="text-justify font-normal text-gray-700">
-                    &quot;{review.review}&quot;
-                  </p>
-                  <div className="h-1 w-16 bg-slate-300"></div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
+              </SwiperSlide>
+            );
+          })}
         </SwiperContainer>
       </div>
     </div>
--- src/data/userReview.ts
+++ src/data/userReview.ts
@@ -1,49 +1,32 @@
 type Review = {
-  image: string;
   review: string;
   name: string;
 };
+
 export const userReviews: Review[] = [
   {
-    image: "/temp/Salwar Kameez.png",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
+      "I got my bridal lehenga from Sanskruti NX, and it's just wow! The embroidery and material are amazing. They have so many options, making it easy to find the perfect outfit. The people there are really nice too. If you're getting married, check them out!",
+    name: "Pooja Sharma",
   },
   {
-    image: "/temp/Indo Western.png",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
+      "Sanskruti NX is where I find my cool kurtis. The designs and colors are so nice. The clothes feel good and fit well. The website is easy to use, and they deliver fast. I always shop here for my Indian clothes.",
+    name: "Aishwarya Singh",
   },
   {
-    image: "/temp/Lehenga's.png",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
+      "I always buy my sarees from Sanskruti NX. They have really good ones. The quality is great, and they tell you everything about the saree. I like that. It's my favorite place for sarees.",
+    name: "Neha Patel",
   },
   {
-    image: "/temp/Kurti's.png",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
+      "I love Sanskruti NX for everyday kurtis. They're nice and not too expensive. The website is easy, and they send your things quickly. I'm happy with what I got!",
+    name: "Ananya Verma",
   },
   {
-    image: "/temp/Bridal.png",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
-  },
-  {
-    image: "/temp/Western Wear.png",
-    review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
-  },
-  {
-    image: "/temp/dressmaterial.png",
-    review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam ipsum cupiditate vero iure officia illo quos dolores repudiandae, nihil ut a repellendus dolor, quas aut, architecto doloremque. Corrupti, quod.",
-    name: "Customer name",
+      "Sanskruti NX is perfect for jewelry. I found the prettiest pieces for my brother's wedding. They're different and made my outfit better. The website is easy, and the people are helpful. I'll go back for sure.",
+    name: "Radhika Gupta",
   },
 ];
